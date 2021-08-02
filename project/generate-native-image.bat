e5e5bab3a4017e61e6d09317e60e6e6a1a1802d8
Alexandre Archambault <alexandre.archambault@gmail.com>
2021-08-02 18:21:47 +0200

    Add dumb script to generate native image on Windows
    It's very heavily fragile… Mill would allow to do the same in a much more
    reliable and straightforward way…

---
+++ project/generate-native-image.bat
@@ -0,0 +1,14 @@
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+if %errorlevel% neq 0 exit /b %errorlevel%
+echo on
+"C:\hostedtoolcache\windows\cs\2.0.13\x64\cs.exe" java --env --jvm-index "https://github.com/coursier/jvm-index/raw/master/index.json" --jvm graalvm-java11:21.2.0
+if %errorlevel% neq 0 exit /b %errorlevel%
+call "D:\a\metabrowse\metabrowse\null\Coursier\cache\jvm\graalvm-java11@21.2.0\bin\gu" install native-image
+if %errorlevel% neq 0 exit /b %errorlevel%
+call sbt ++%SCALA_VERSION% "show server-cli/nativeImageManifest"
+if %errorlevel% neq 0 exit /b %errorlevel%
+call "D:\a\metabrowse\metabrowse\null\Coursier\cache\jvm\graalvm-java11@21.2.0\bin\native-image.cmd" -cp metabrowse-server-cli\target\native-image-internal\manifest.jar metabrowse.server.cli.MetabrowseServerCli server-cli
+if %errorlevel% neq 0 exit /b %errorlevel%
+mkdir artifacts\
+xcopy server-cli.exe artifacts\
+if %errorlevel% neq 0 exit /b %errorlevel%
